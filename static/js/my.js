2892ef465d76902a05dbbd54873de9f729eb0228
Pável Varela Rodríguez <neonskull@gmail.com>
2016-06-05 21:12:29 -0500

    improved .on();added .css()
    - Improved function .on() and added an Array version, to make it easy to add events to arrays of elements
    - Added new function .css(), also similar to jQuery's .css()

--- static/js/my.js
+++ static/js/my.js
@@ -1,8 +1,44 @@
 /* your js code here */
+
+// doing 'return this;' on every function so I can chain them
+// Sizzle('ul#toolbar li')
+// 		.on('click', function() { console.log(this.innerText); })
+//  	.css({'size': '1.3em'});
+
 Object.prototype.on = function (evnt, funct) {
-	return (this.attachEvent)
-		? this.attachEvent('on' + evnt, funct)
-		: this.addEventListener(evnt, funct, false);
+	if (this.attachEvent) {
+		this.attachEvent('on' + evnt, funct)
+	} else {
+		this.addEventListener(evnt, funct, false);
+	}
+
+	return this;
+}
+
+Array.prototype.on = function (evnt, funct) {
+	this.forEach(function(element) {
+		element.on(evnt, funct);
+	});
+
+	return this;
+}
+
+Object.prototype.css = function (cssRules) {
+	for(var attr in cssRules) {
+		if(typeof(cssRules[attr]) !== "function") {
+			this.style[attr] = cssRules[attr];
+		}
+	}
+
+	return this;
+}
+
+Array.prototype.css = function (cssRules) {
+	this.forEach(function(element) {
+		element.css(cssRules);
+	});
+
+	return this;
 }
 
 function fixExternalLinksTarget()
